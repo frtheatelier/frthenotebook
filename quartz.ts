057cc04fc0671c5ea922dd3859b93ed3d366a0c3
Kimberly Prijadi <prijadikimberly@gmail.com>
2026-06-01 15:02:18 -0400

    Quartz sync: Jun 1, 2026, 3:02 PM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,44 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-const config = await loadQuartzConfig()
+const config = await loadQuartzConfig({
+  theme: {
+      typography: {
+          header: {
+              name: "Stack Sans Headline",
+              weights: [400, 700],
+              includeItalic: false,
+          },
+          body: "Stack Sans Text",
+          code: "IBM Plex Mono",
+      },
+      cdnCaching: false,
+      fontOrigin: "googleFonts",
+      colors: {
+        lightMode: {
+            light: "#faf8f8",
+            lightgray: "#e5e5e5",
+            gray: "#b8b8b8",
+            darkgray: "#4e4e4e",
+            dark: "#2b2b2b",
+            secondary: "#284b63",
+            tertiary: "#84a59d",
+            highlight: "#8f9fa926",
+            textHighlight: "#fff23688"
+        },
+        darkMode: {
+            light: "#161618",
+            lightgray: "#393639",
+            gray: "#646464",
+            darkgray: "#d4d4d4",
+            dark: "#ebebec",
+            secondary: "#7b97aa",
+            tertiary: "#84a59d",
+            highlight: "#8f9fa926",
+            textHighlight: "#b3aa0288"
+        }
+      }
+  },
+})
+
 export default config
 export const layout = await loadQuartzLayout()
